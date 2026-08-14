adb50d886e0450222b1e08f8df473c0b570725ae
thearyan-044 <thearyan-044@users.noreply.github.com>
2026-08-14 16:14:18 +0530

    Update preregistration configuration

--- pre-registration landing page/config.js
+++ pre-registration landing page/config.js
@@ -1,5 +1,5 @@
 window.PDS_CONFIG = Object.freeze({
   // Paste the deployed Google Apps Script Web App URL here.
   // It must end in /exec, not /dev.
-  googleSheetsWebAppUrl: ""
+  googleSheetsWebAppUrl: "https://script.google.com/macros/s/AKfycby_oxg0hFLDaW_7pxoqwBLDvqbkmUPZ5wMsNWXocKULssePGDh7hG6_Rsb2hRP4djcBew/exec"
 });
